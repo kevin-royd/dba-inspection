e1e9a0c7e1c6ee41fbf07aca5f8bfe9e307b0036
ludelice <ludelice@gmail.com>
2022-09-17 01:02:33 +0800

    修改日志错误路径名称

--- centos/check_os.py
+++ centos/check_os.py
@@ -5,7 +5,7 @@ import re
 
 
 def check_numa():
-    log = logger.logger_config('err', 'check_numa')
+    log = logger.logger_config('../log/err.log', 'check_numa')
     # 首先判断是否启动了numa
     cmd = 'dmesg | grep -i numa'
     message = cmd_util.exec_cmd(cmd)
@@ -16,12 +16,13 @@ def check_numa():
         # 校验上条命令是否有报错 因为sed命令无返回结果。如果有返回肯定是错误信息
         if cmd_util is not None:
             log.error('请检查文件是否存在：/etc/default/grub')
-        print('numa关闭成功,重启机器生效')
+        else:
+            print('numa关闭成功,重启机器生效')
 
 
 # 通过判断swap空间大小来判断是否启用
 def check_swap():
-    log = logger.logger_config('err.log', 'check_swap')
+    log = logger.logger_config('../log/err.log', 'check_swap')
     cmd = "free -m | grep -i 'swap' | awk '{print $2}'"
     message = cmd_util.exec_cmd(cmd)
     # 对返回结果进行处理 b'0\n'
@@ -38,3 +39,4 @@ def check_swap():
         message_buffer = cmd_util.exec_cmd(cmd_swap)
         if message_buffer is not None:
             log.error('关闭swap出错')
+        print('')
