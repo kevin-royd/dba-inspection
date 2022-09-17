60b51ef9de5eda40ba32eac1ba92a3bc8235838b
ludelice <ludelice@gmail.com>
2022-09-17 14:33:53 +0800

    增加class类方便全局调用，

--- centos/check_os.py
+++ centos/check_os.py
@@ -1,56 +1,66 @@
 from cmdUtile import cmd_util, logger
+from raid import check_raid
 import re
 
 
-def check_numa():
-    log = logger.Logger().get_log
-    # 首先判断是否启动了numa
-    cmd = 'dmesg | grep -i numa=off'
-    message = cmd_util.exec_cmd(cmd)  # 管道符执行才会有结果输出
-    if message is None:
-        # 判断内核文件是否已经添加了numa=off
-        cmd_grep_numa = 'cat /etc/default/grub | grep numa=off'
-        numa_message = cmd_util.exec_cmd(cmd_grep_numa)
-        # 如果为空表示未修改
-        if numa_message is None:
-            print('numa未关闭,执行修改内核参数')
-            cmd_numa = "sed -i 's#biosdevname=0#biosdevname=0 numa=off#' /etc/default/grub"
-            cmd_util.exec_cmd(cmd_numa)
-            # 校验上条命令是否有报错 因为sed命令无返回结果。如果有返回肯定是错误信息
-            if cmd_util is not None:
-                log.error('请检查文件是否存在：/etc/default/grub')
+class CentOS(object):
+    def __init__(self):
+        self.log = logger.Logger().get_log
+        self.disk_name = check_raid.Raid().get_disk_name()
+
+    def check_numa(self):
+
+        # 首先判断是否启动了numa
+        cmd = 'dmesg | grep -i numa=off'
+        message = cmd_util.exec_cmd(cmd)  # 管道符执行才会有结果输出
+        if message is None:
+            # 判断内核文件是否已经添加了numa=off
+            cmd_grep_numa = 'cat /etc/default/grub | grep numa=off'
+            numa_message = cmd_util.exec_cmd(cmd_grep_numa)
+            # 如果为空表示未修改
+            if numa_message is None:
+                print('numa未关闭,执行修改内核参数')
+                cmd_numa = "sed -i 's#biosdevname=0#biosdevname=0 numa=off#' /etc/default/grub"
+                cmd_util.exec_cmd(cmd_numa)
+                # 校验上条命令是否有报错 因为sed命令无返回结果。如果有返回肯定是错误信息
+                if cmd_util is not None:
+                    self.log.error('请检查文件是否存在：/etc/default/grub')
+                else:
+                    print('numa关闭成功,重启机器生效')
+                    # 需要重新读取配hi在
+                    cmd_reload = 'grub2-mkconfig -o /boot/grub2/grub.cfg'
+                    cmd_util.exec_cmd(cmd_reload)
             else:
-                print('numa关闭成功,重启机器生效')
+                print('numa已关闭,重启机器生效')
                 # 需要重新读取配hi在
                 cmd_reload = 'grub2-mkconfig -o /boot/grub2/grub.cfg'
                 cmd_util.exec_cmd(cmd_reload)
         else:
-            print('numa已关闭,重启机器生效')
-            # 需要重新读取配hi在
-            cmd_reload = 'grub2-mkconfig -o /boot/grub2/grub.cfg'
-            cmd_util.exec_cmd(cmd_reload)
-    else:
-        print('numa已关闭')
+            print('numa已关闭')
 
+    # 通过判断swap空间大小来判断是否启用
+    def check_swap(self):
+        cmd = "free -m | grep -i 'swap' | awk '{print $2}'"
+        message = cmd_util.exec_cmd(cmd)
+        # 对返回结果进行处理 b'0\n'
+        result = int(re.search('.*([0-9]+)', str(message)).group(1))
+        if result != 0:
+            print('未关闭swap。现在开始关闭swap')
+            cmd_buffer = 'echo 3 > /proc/sys/vm/drop_caches'
+            # 该命令也无返回值 有返回值也报错
+            message_buffer = cmd_util.exec_cmd(cmd_buffer)
+            if message_buffer is not None:
+                self.log.error('清空系统缓存错误')
+            # 关闭swap
+            cmd_swap = 'swapoff -a'
+            message_buffer = cmd_util.exec_cmd(cmd_swap)
+            if message_buffer is not None:
+                self.log.error('关闭swap出错')
+        print('swap已关闭')
 
-# 通过判断swap空间大小来判断是否启用
-def check_swap():
-    log = logger.Logger().get_log
-
-    cmd = "free -m | grep -i 'swap' | awk '{print $2}'"
-    message = cmd_util.exec_cmd(cmd)
-    # 对返回结果进行处理 b'0\n'
-    result = int(re.search('.*([0-9]+)', str(message)).group(1))
-    if result != 0:
-        print('未关闭swap。现在开始关闭swap')
-        cmd_buffer = 'echo 3 > /proc/sys/vm/drop_caches'
-        # 该命令也无返回值 有返回值也报错
-        message_buffer = cmd_util.exec_cmd(cmd_buffer)
-        if message_buffer is not None:
-            log.error('清空系统缓存错误')
-        # 关闭swap
-        cmd_swap = 'swapoff -a'
-        message_buffer = cmd_util.exec_cmd(cmd_swap)
-        if message_buffer is not None:
-            log.error('关闭swap出错')
-    print('swap已关闭')
+    def check_io_scheduler(self):
+        # 获取磁盘名称
+        # 获取磁盘支持的io调度器和当前使用的调度器
+        cmd_get_io_scheduler = f'cat /sys/block/{self.disk_name}/queue/scheduler'
+        io_scheduler = cmd_util.exec_cmd(cmd_get_io_scheduler)
+        print(io_scheduler)
--- raid/check_raid.py
+++ raid/check_raid.py
@@ -1,14 +1,22 @@
 from cmdUtile import cmd_util
 
 
-def check_raid_all():
+class Raid(object):
+
+    def __init__(self):
+        self.disk_name = ''
+
+    def get_disk_name(self):
+        cmd = 'fdisk -l'
+        data = cmd_util.exec_cmd(cmd)
+        # 获取磁盘名称
+        self.disk_name = str(data).split(' ')[1].split(':')[0]
+        return self.disk_name
+
     # 检擦磁盘坏道
-    cmd = 'fdisk -l'
-    data = cmd_util.exec_cmd(cmd)
-    # 获取磁盘名称
-    disk_name = str(data).split(' ')[1].split(':')[0]
-    # 磁盘只读检测
-    cmd2 = f'badblocks -s -v {disk_name}'
-    bad = cmd_util.exec_cmd(cmd2)
-    if bad is None:
-        print('没有坏道')
+    def check_raid_all(self):
+        # 磁盘只读检测
+        cmd2 = f'badblocks -s -v {self.disk_name}'
+        bad = cmd_util.exec_cmd(cmd2)
+        if bad is None:
+            print('没有坏道')
